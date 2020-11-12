12bf0361b17bc59340429bd6ca0e4625f23247b1
KuroNeko <kuroneko9505@gmail.com>
2020-11-12 16:23:30 +0900

    API update - snapshot

---
+++ lib/api/snapshot.dart
@@ -0,0 +1,37 @@
+import 'package:VultrDash/api/api.dart';
+import 'package:VultrDash/api/model/snapshot.dart';
+
+class Snapshot extends API {
+  Future<SnapshotsModel> getInfo() {
+    return this
+        .Get("/snapshots", null)
+        .then((value) => SnapshotsModel.fromJson(value));
+  }
+
+  Future deleteSnapshot(String snapshotId) {
+    return this.Delete("/snapshots/$snapshotId", null, null);
+  }
+
+  Future<SnapshotModel> getSnapshot(String snapshotId) {
+    return this
+        .Get("/snapshots/$snapshotId", null)
+        .then((value) => SnapshotModel.fromJson(value["snapshot"]));
+  }
+
+  Future updateSnapshot(String snapshotId, String description) {
+    return this
+        .Put("/snapshots/$snapshotId", null, {"description": description});
+  }
+
+  Future<SnapshotModel> createSnapshot(String instanceId, String description) {
+    return this.Post("/snapshots", null, {
+      "instanceId": instanceId,
+      "description": description
+    }).then((value) => SnapshotModel.fromJson(value["snapshot"]));
+  }
+
+  Future<SnapshotModel> createSnapshotFromURL(String url) {
+    return this.Post("/snapshots/create-from-url", null, {"url": url}).then(
+        (value) => SnapshotModel.fromJson(value["snapshot"]));
+  }
+}
